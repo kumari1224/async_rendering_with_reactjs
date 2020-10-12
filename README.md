4097a556d72cf7c9e80f4cdf7b2bbc22c11d901c
priyanka <priyanka.kumari@tathastu.ai>
2020-10-13 00:43:40 +0530

    Commit 1: Added code to page1, page2, page3

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # async_rendering_with_reactjs
+### Observations
+### Commit 1
+* In this case we are loading all the components at once. 
+* Bundle.js : ~6.7 kb
+* 0.chunk : ~383 kb
+* main.chunk : ~4.5 kb
+
+### Commit 2
+
+### Commit 3
--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Page1 from './Components/Page1';
+import Page2 from './Components/Page2';
+import Page3 from './Components/Page3';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    route: "page1",
+  };
+  onRouteChange = (route) => {
+    this.setState({ route: route });
+  };
+  render() {
+    if (this.state.route === "page1") {
+      return <Page1 onRouteChange={this.onRouteChange} />;
+    } else if (this.state.route === "page2") {
+      return <Page2 onRouteChange={this.onRouteChange} />;
+    } else {
+      return <Page3 onRouteChange={this.onRouteChange} />;
+    }
+  }
 }
 
 export default App;
