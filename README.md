6a5defc7dcfc0f6cc4a96e1ae7f4ca2f7ac3b737
priyanka <priyanka.kumari@tathastu.ai>
2020-10-13 11:40:41 +0530

    Commit 4: Used Suspense and Lazy load

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # async_rendering_with_reactjs
+##### Note : Read react's official doc[https://reactjs.org/docs/code-splitting.html]
+
 ### Observations
 #### Commit 1
 * In this case we are loading all the components at once. 
@@ -20,4 +22,6 @@
 * To see the loader component you should go to Network tab -> select Slow 3G.
 
 #### Commit 4
-* 
+* In this case we are using inbuild features of React16 React.lazy and Suspense.
+* React.lazy takes a function that must call a dynamic import(). This must return a Promise which resolves to a module with a default export containing a React component.
+* The lazy component should then be rendered inside a Suspense component, which allows us to show some fallback content (such as a loading indicator) while we’re waiting for the lazy component to load.
--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import "./App.css";
 import Page1 from "./Components/Page1";
-import asyncComponentWrapper from "./Components/asyncComponent";
+import Loader from "./Components/Loader";
+
+const Page2Lazy = lazy(() => import("./Components/Page2.js"));
+const Page3Lazy = lazy(() => import("./Components/Page3.js"));
 
 class App extends React.Component {
   state = {
     route: "Page1",
     component: "",
   };
+
   onRouteChange = (route) => {
     this.setState({ route });
   };
+
   render() {
     if (this.state.route === "Page1") {
       return <Page1 onRouteChange={this.onRouteChange} />;
-    } else {
-      const AsyncComponent = asyncComponentWrapper(() =>
-        import(`./Components/${this.state.route}.js`)
+    } else if (this.state.route === "Page2") {
+      return (
+        <Suspense fallback={<Loader />}>
+          <Page2Lazy onRouteChange={this.onRouteChange} />
+        </Suspense>
       );
-      return <AsyncComponent onRouteChange={this.onRouteChange} />;
+    } else {
+      return (
+        <Suspense fallback={<Loader />}>
+          <Page3Lazy onRouteChange={this.onRouteChange} />
+        </Suspense>
+      );
     }
   }
 }
